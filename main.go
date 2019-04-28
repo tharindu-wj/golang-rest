3611a5df525c9a60eb8514d810c92a0808963ee4
Tharindu <tharinduw@eight25media.com>
2019-04-28 18:48:55 +0530

    article creat function added comments added for functions

--- main.go
+++ main.go
@@ -4,40 +4,90 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+//json file name
+var jsonFile = "articles.json"
+
+//single article struct
 type Article struct {
-	Title   string `json:"Title"`
-	Desc    string `json:"Desc"`
-	Content string `json:"Content"`
+	ID      int      `json:"ID"`
+	Title   string   `json:"Title"`
+	Content string   `json:"Content"`
+	Tags    []string `json:"Tags"`
 }
 
+//article list struct
 type Articles []Article
 
-func allArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Articles{
-		Article{Title: "Test Title", Desc: "Test Description", Content: "Hello World"},
+//read json and return articles slice
+func readJsonFile() Articles {
+	plan, _ := ioutil.ReadFile(jsonFile)
+	var articles Articles
+	err := json.Unmarshal(plan, &articles)
+	if err != nil {
+		fmt.Print(err)
 	}
-
-	fmt.Println("Endpoint All Articles")
-	json.NewEncoder(w).Encode(articles)
+	return articles
 }
 
+//return all articles as json format
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := readJsonFile()
+
+	fmt.Println("Endpoint All Articles")
+	err := json.NewEncoder(w).Encode(articles)
+
+	if err != nil {
+		fmt.Print(err)
+	}
+}
+
+//creat new article by post request
+func createArticle(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	title := r.Form.Get("title")
+	content := r.Form.Get("content")
+
+	newArticle := Article{
+		ID:      3,
+		Title:   title,
+		Content: content,
+		Tags:    []string{"tag1", "tag2"},
+	}
+
+	oldArticles := readJsonFile()
+	newArticles := append(oldArticles, newArticle)
+
+	articleJson, _ := json.Marshal(newArticles)
+	err := ioutil.WriteFile(jsonFile, articleJson, 0644)
+	if err != nil {
+		fmt.Print(err)
+	} else {
+		fmt.Fprintf(w, "New Article Created")
+	}
+}
+
+//home page route
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage Endpoint")
 }
 
+//http request handler developed using mux package
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
+	myRouter.HandleFunc("/article/create", createArticle).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
+//program starts from here
 func main() {
 	handleRequests()
 }
